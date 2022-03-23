4ee2eb055ae1e4866bd26bf105343754a1d81456
CarlosBitizin <carlospintorossy07@gmail.com>
2022-03-22 20:42:57 -0400

    Structuring the Project

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container, Title } from './styles';
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Title>BItzada</Title>
+        </Container>
+    )
+}
