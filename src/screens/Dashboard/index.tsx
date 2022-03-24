02735bc5e1ffbe76c9b77467d6da182dfdc6a7af
CarlosBitizin <carlospintorossy07@gmail.com>
2022-03-24 11:48:59 -0400

    creating the font theme

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { Container, Title } from './styles';
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>BItzada</Title>
+            <Title>Dashboard</Title>
         </Container>
     )
 }
